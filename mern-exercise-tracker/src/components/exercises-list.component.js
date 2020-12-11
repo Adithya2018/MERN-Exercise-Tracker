633a94cb459df65edfabf5265dd39be05895f1cd
Adithyan S Raj <adithyansraj20@gmail.com>
2020-12-11 19:43:24 +0530

    19th commit

--- mern-exercise-tracker/src/components/exercises-list.component.js
+++ mern-exercise-tracker/src/components/exercises-list.component.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class ExercisesList extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.deleteExercise = this.deleteExercise.bind(this);
+
+        this.state = {exercises: []};
+
+
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:5000/exercises/')
+        .then(response => {
+            this.setState ({ exercises: response.data })
+        })
+    }
+
     render(){
         return(
             <div>
-                <p>You are on Exercises List component</p>
+                
             </div>
         );
     }
